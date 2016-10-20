c73f0904b3d2dfe6573fbd037115cacd05e827f4
82140292 <82140292@qq.com>
2016-10-20 17:36:50 +0800

    完善新鲜事，显示新鲜事界面

--- app/src/main/java/com/lijianping/jiandan/adapter/FreshNewsAdapter.java
+++ app/src/main/java/com/lijianping/jiandan/adapter/FreshNewsAdapter.java
@@ -59,15 +59,6 @@ public class FreshNewsAdapter extends RecyclerView.Adapter<FreshNewsAdapter.View
         this.mLoadResuleCallBack = loadResultCallBack;
         mFreshNews = new ArrayList<>();
 
-//        for (int i = 0; i < 10; i ++){
-//            FreshNews freshNews = new FreshNews();
-//            freshNews.setId(123 + "");
-//            freshNews.setUrl("http://i.jandan.net/2016/10/18/hold-your-pee.html");
-//            freshNews.setTitle("煎蛋小学堂：憋尿的后果会有多严重？");
-//            freshNews.setDate("2016-10-18 21:27:49");
-//            mFreshNews.add(freshNews);
-//        }
-
         int loadingResource = isLargeMode ? R.drawable.ic_loading_large : R.drawable.ic_loading_small;
         options = ImageLoadProxy.getOptions4PictureList(loadingResource);
     }
